3f6060df6d76316c49ad2976e7797df0d88b1763
Lenin Moposita <leninmopositacrespogmail.com>
2022-05-30 19:02:32 -0500

    Fecha 30/05/2022 Hora 19:00

--- app/src/main/java/com/example/myapplication/BMLMActivity1.java
+++ app/src/main/java/com/example/myapplication/BMLMActivity1.java
@@ -3,8 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class BMLMActivity1 extends AppCompatActivity {
+ private ListView lstOrdenar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
